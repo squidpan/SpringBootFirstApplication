6e22a9ce1bf20e924af0f6112e5c12f0f87b90e1
squidpan <squidpan11@gmail.com>
2025-12-04 14:59:29 -0500

    rev
    rev

--- src/main/java/com/squidpan/app/SpringBootFirstApplication.java
+++ src/main/java/com/squidpan/app/SpringBootFirstApplication.java
@@ -2,17 +2,19 @@ package com.squidpan.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstApplication {
 
 	public static void main(String[] args) {
-
-        SpringApplication.run(SpringBootFirstApplication.class, args);
         System.out.println("Hello World!");
+        ApplicationContext context=SpringApplication.run(SpringBootFirstApplication.class, args);
+        Alien obj = context.getBean(Alien.class);
+        obj.code();
 
-        Laptop laptop = new Laptop();
-        laptop.compile();
+        //Laptop laptop = new Laptop();
+        //laptop.compile();
 	}
 
 }
