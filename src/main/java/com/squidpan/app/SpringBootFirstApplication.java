cc4d0a65f43ceba9a2c2222bcf1d30506725d838
squidpan <squidpan11@gmail.com>
2025-12-04 12:16:36 -0500

    Initial commit

---
+++ src/main/java/com/squidpan/app/SpringBootFirstApplication.java
@@ -0,0 +1,15 @@
+package com.squidpan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootFirstApplication.class, args);
+        System.out.println("Hello World!");
+	}
+
+}
